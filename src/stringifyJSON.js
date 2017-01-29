ee85946591017735c2e2a96d2b3cf65e1cba9519
Dario Arteaga <daartv@gmail.com>
2017-01-29 15:12:04 -0400

    Test passed for Stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var arr = []; 
+	var	values = [];
+	var	props = [];
+  if( typeof obj === 'boolean' || typeof obj === 'number' || obj === null)
+  	return '' + obj;
+  else if (typeof obj === 'string')
+  	return '"' + obj + '"';
+  else if (Array.isArray(obj)){
+  	if(obj[0] === undefined)
+		return '[]';
+	else {
+		obj.forEach(function(element){
+			values.push(stringifyJSON(element));
+		});
+		return '[' + values + ']'
+	}  
+  }
+  else if (obj instanceof Object){
+  	props = Object.keys(obj);
+  	props.forEach(function(key){
+  		var strKey = '"' + key + '":';
+  		var value = obj[key];
+  		if(value instanceof Function || typeof value === undefined)
+  			arr.push('');
+  		else if(typeof value === 'string')
+  			arr.push(strKey + '"' + value + '"');
+  		else if(typeof value === 'number' || typeof value === 'boolean' ||  value === null)
+  			arr.push(strKey + value);
+  		else if (value instanceof Object){
+  			arr.push(strKey + stringifyJSON(value));	
+  		}
+  	});
+  	return '{' + arr + '}';
+  }
 };
