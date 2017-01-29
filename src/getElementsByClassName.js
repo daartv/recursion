ac7d2c7c286c1288027685b03913fd941cd6a055
Dario Arteaga <daartv@gmail.com>
2017-01-29 15:10:29 -0400

    Test passed for GetElementsClass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+	var result = [];
+	function auxFn (n) {
+		var classes = n.className.split(' ');
+		for (var i=0; i<classes.length; i++) {
+			if (classes[i] === className) {
+				result.push(n);
+			}
+		}
+		var children = n.children;
+		for (var j=0; j<children.length; j++) {
+			auxFn(n.children[j]);
+		}
+	};
+	auxFn(document.body);
+	return result;
 };
